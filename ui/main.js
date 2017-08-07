d1da4962fe0d5b002db2396b3ccc3340904bed0b
Aishwarya-Holla <30688327+Aishwarya-Holla@users.noreply.github.com>
2017-08-07 13:52:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,17 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
 
-var element= document.getElementById('main-text');
-element.innerHTML='New Value';
-
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+button.onclick = function() {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+      if(request.readystate=== XMLHttpRequest.DONE) {
+          if(request.status===200) {
+             var counter =  request.responseText;
+              var span = document.getElementById('count');
+               span.innerHTML = counter.toString();
+          }
+      }
+      };
+  request.open('GET','http://lakshmiaishwarya11.imad.hasura-app.io/counter', true);
+  request.send(null);
+ 
 };
